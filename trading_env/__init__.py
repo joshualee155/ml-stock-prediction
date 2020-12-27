33d5ec8650c1d9e97912c0b8556ff01e4b163d96
Joshua Lee <hpcq_124@163.com>
2020-12-27 13:49:56 +0000

    update dqn, add discrete version environment

--- trading_env/__init__.py
+++ trading_env/__init__.py
@@ -9,6 +9,10 @@ register(
     entry_point='trading_env.envs:TestTradingEnv',
 )
 register(
+    id='test_trading-v1',
+    entry_point='trading_env.envs:TestTradingEnvV1',
+)
+register(
     id='test_trading-v2',
-    entry_point='trading_env.envs:TestTradingEnv',
+    entry_point='trading_env.envs:TestTradingEnvV2',
 )
--- trading_env/envs/__init__.py
+++ trading_env/envs/__init__.py
@@ -1,2 +1,3 @@
 # from trading_env.envs.trading_env import TradingEnv
-from trading_env.envs.trading_env_v2 import TestTradingEnv
+from trading_env.envs.trading_env_v2 import TestTradingEnv as TestTradingEnvV2
+from trading_env.envs.trading_env_v1 import TestTradingEnv as TestTradingEnvV1
